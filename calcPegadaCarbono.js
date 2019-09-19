20fa8de73e5754fd75381e36ca8e75effc4ba087
JuniorDuarte93 <jr.duartee.93@gmail.com>
2019-09-18 22:37:00 -0300

    teste

---
+++ calcPegadaCarbono.js
@@ -0,0 +1,67 @@
+function calcPegadaCarbono(energiaKwhMes,
+    gasCozinhaKgMes,
+    gasNaturalM3Mes,
+    veicPGasolinaKmMes,
+    veicMGasolinaKmMes,
+    veicGGasolinaKmMes,
+    motoGasolinaKmMes,
+    veicPEtanolKmMes,
+    veicMEtanolKmMes,
+    veicGEtanolKmMes,
+    motoEtanolKmMes,
+    carroDieselKmMes,
+    onibusDieselUrbanoKmMes,
+    onibusDieselRodoviaKmMes,
+    carroGnvKmMes,
+    viagemNacionalKmMes,
+    viagemInternacionalKmMes,
+    aterroResiduosKgDia) {
+
+    let TotalEmissaoTonCO2 = parseFloat(
+        energiaKwhMes * 0.295 +
+        gasCozinhaKgMes * 35.893 +
+        gasNaturalM3Mes * 22.674 +
+        veicPGasolinaKmMes * 2.184 +
+        veicMGasolinaKmMes * 2.579 +
+        veicGGasolinaKmMes * 3.571 +
+        motoGasolinaKmMes * 1.268 +
+        veicPEtanolKmMes * 0.7190001 +
+        veicMEtanolKmMes * 0.7190001 +
+        veicGEtanolKmMes * 0.7190001 +
+        motoEtanolKmMes * 0.7190001 +
+        carroDieselKmMes * 3.091 +
+        onibusDieselUrbanoKmMes * 2.28 +
+        onibusDieselRodoviaKmMes * 0.6 +
+        carroGnvKmMes * 3.128 +
+        viagemNacionalKmMes * 0.100188 +
+        viagemInternacionalKmMes * 0.112465 +
+        aterroResiduosKgDia * 963.6
+    )
+
+    return {
+        energiaKwhMes: energiaKwhMes,
+        gasCozinhaKgMes: gasCozinhaKgMes,
+        gasNaturalM3Mes: gasNaturalM3Mes,
+        veicPGasolinaKmMes: veicPGasolinaKmMes ,
+        veicMGasolinaKmMes: veicMGasolinaKmMes,
+        veicGGasolinaKmMes: veicGGasolinaKmMes,
+        motoGasolinaKmMes: motoGasolinaKmMes,
+        veicPEtanolKmMes: veicPEtanolKmMes,
+        veicMEtanolKmMes: veicMEtanolKmMes,
+        veicGEtanolKmMes: veicGEtanolKmMes,
+        motoEtanolKmMes: motoEtanolKmMes,
+        carroDieselKmMes: carroDieselKmMes,
+        onibusDieselUrbanoKmMes: onibusDieselUrbanoKmMes,
+        onibusDieselRodoviaKmMes: onibusDieselRodoviaKmMes,
+        carroGnvKmMes: carroGnvKmMes,
+        viagemNacionalKmMes: viagemNacionalKmMes,
+        viagemInternacionalKmMes: viagemInternacionalKmMes,
+        aterroResiduosKgDia:aterroResiduosKgDia,
+
+        TotalEmissaoTonCO2: TotalEmissaoTonCO2,
+        Teste: "teste"
+    }
+}
+
+
+module.exports = calcPegadaCarbono
---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const calcPegadaCarbono = require('./calcPegadaCarbono')
+
+//app.use(bodyParser.text())
+//app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.post('/pegadacarbono', (req, res) => {
+
+    result = calcPegadaCarbono(
+        req.body.energiaKwhMes,
+        req.body.gasCozinhaKgMes,
+        req.body.gasNaturalM3Mes,
+        req.body.veicPGasolinaKmMes,
+        req.body.veicMGasolinaKmMes,
+        req.body.veicGGasolinaKmMes,
+        req.body.motoGasolinaKmMes,
+        req.body.veicPEtanolKmMes,
+        req.body.veicMEtanolKmMes,
+        req.body.veicGEtanolKmMes,
+        req.body.motoEtanolKmMes,
+        req.body.carroDieselKmMes,
+        req.body.onibusDieselUrbanoKmMes,
+        req.body.onibusDieselRodoviaKmMes,
+        req.body.carroGnvKmMes,
+        req.body.viagemNacionalKmMes,
+        req.body.viagemInternacionalKmMes,
+        req.body.aterroResiduosKgDia
+    )
+    res.send(result)
+})
+
+
+
+app.post('/corpo', (req, res) => {
+    // let corpo = ''
+    //req.on('data', function (parte) {
+    //    corpo += parte
+    // })
+
+    // req.on('end', function () {
+    //    res.json(corpo)
+    //})
+
+    res.send(req.body)
+
+})
+
+
+app.get('/clientes/relatorio', (req, res) => {
+    res.send(`Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`)
+})
+
+app.get('/clientes/:id', (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado!`)
+})
+
+app.get('/opa', (req, res) => {
+
+    res.json({
+
+        data: [
+            { id: 1, name: 'Jose', position: 1 },
+            { id: 34, name: 'Junior', position: 2 },
+            { id: 80, name: 'Joice', position: 3 }
+        ],
+        count: 30,
+        skip: 10,
+        limit: 3,
+        statys: 200
+    })
+
+
+    // res.json({
+    //  name: "Iphone 11",
+    //     price:  6000.00,
+    //  discount: 0.12
+    //})
+
+    //res.send("Estou bem!")
+})
+
+app.listen(3000, () => {
+    console.log('Iniciado')
+})
